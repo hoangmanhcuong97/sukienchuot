7d6515fdc4242a6c3278920dd499e4d176714aa9
hoangmanhcuong97 <hoangmanhcuong01997@gmail.com>
2021-06-11 15:08:58 +0700

    di chuyen anh

---
+++ moveIMG.js
@@ -0,0 +1,13 @@
+let nhanvat = 10;
+function init(){
+  nhanvat = document.getElementById("contra");
+  nhanvat.style.position = "relative";
+  nhanvat.style.left = "0px";
+}
+function moveRight (){
+  nhanvat.style.left = parseInt(nhanvat.style.left) + 50 + "px";
+  }
+function moveLeft (){
+  nhanvat.style.left = parseInt(nhanvat.style.left) - 50 + "px";
+}
+window.onload =init();
